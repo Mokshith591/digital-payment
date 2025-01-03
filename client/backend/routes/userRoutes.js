fb232efa40eea677fb2b42258c828a4f13312141
Mokshith M C <132329445+Mokshith591@users.noreply.github.com>
2025-01-03 20:29:14 +0530

    Delete client directory

--- client/backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getUserDetails } = require("../controllers/userController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-const router = express.Router();
-router.get("/", authMiddleware, getUserDetails);
-
-module.exports = router;
